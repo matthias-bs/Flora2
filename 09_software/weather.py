8fdce32fbd371091e2e8b6eb07a5fdecc60745b0
Matthias Prinke <m.prinke@arcor.de>
2025-03-05 13:58:09 +0100

    Added M5Stack ENV III sensor

--- 09_software/weather.py
+++ 09_software/weather.py
@@ -17,6 +17,8 @@
 # History:
 #
 # 20210521 Created
+# 20250305 Added M5Stack ENV III sensor
+#          (http://docs.m5stack.com/en/hat/hat_envIII)
 #
 # ToDo:
 # - 
@@ -27,29 +29,52 @@ import config as cfg
 from print_line import *
 
 from machine import Pin
-from machine import I2C, SoftI2C
+from machine import SoftI2C
 import bme280
+import sht30 # https://github.com/cdrajb/M5_ENVIII
+import qmp6988 # https://github.com/cdrajb/M5_ENVIII
 
 def weather_data():
-    valid = True
+    data = {}
+    valid = False
     
     try:
         bus = SoftI2C(scl=Pin(cfg.GPIO_I2C_SCL), sda=Pin(cfg.GPIO_I2C_SDA))
     except OSError as exc:
         valid = False
-        print_line('I2C Bus Error! ({})!'.format(exc.args[1]), error=True, console=True, sd_notify=True)
+        print_line(f'I2C Bus Error! ({exc.args[1]})!', error=True, console=True, sd_notify=True)
     
     try:
         bme = bme280.BME280(i2c=bus, address=cfg.BME280_ADDR, mode=bme280.BME280_OSAMPLE_1)
     except OSError as exc:
-        valid = False
-        print_line('Failed to access BME280 sensor at I2C address {}!'.format(hex(cfg.BME280_ADDR)), 
-                    error=True, console=True, sd_notify=True)
-
-    data = {}
-    if valid:
+        print_line(f'Failed to access BME280 sensor at I2C address {hex(cfg.BME280_ADDR)}!', 
+                   error=True, console=True, sd_notify=True)
+    else:
+        valid = True
         data['temperature'] = round(bme.temperature(), 1)
         data['pressure']    = round(bme.pressure(), 0)
         data['humidity']    = round(bme.humidity(), 0)
-    
+
+    try:
+        sht = sht30.SHT30(i2c=bus)
+    except OSError as exc:
+        print_line(f'Failed to access M5Stack ENV III SHT30 sensor!',
+                   error=True, console=True, sd_notify=True)
+    else:
+        valid = True
+        temperature, humidity = sht.measure()
+        data['temperature'] = round(temperature, 1)
+        data['humidity']    = round(humidity, 0)
+        
+    try:
+        qmp = qmp6988.QMP6988(i2c=bus)
+    except OSError as exc:
+        valid = False
+        print_line(f'Failed to access M5Stack ENV III QMP6988 sensor!',
+                   error=True, console=True, sd_notify=True)
+    else:
+        valid = True
+        _, pressure = qmp.measure()
+        data['pressure']    = round(pressure / 100.0, 0)
+
     return (valid, data)
