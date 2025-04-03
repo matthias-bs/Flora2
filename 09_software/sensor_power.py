5410b8622d47c43581cdaec529874def8fb30564
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2025-04-03 19:10:45 +0200

    Update sensor_power.py

--- 09_software/sensor_power.py
+++ 09_software/sensor_power.py
@@ -50,10 +50,10 @@ class SensorPower:
         self.p_power = pin_sensor_power
         self.status = 0
         if sys.platform == "esp32":
-            self.pin_power = Pin(self.p_power, Pin.OUT, value = 0)
+            self.pin_power = Pin(self.p_power, Pin.OUT, value = 0) # pylint: disable=E0606
         else:
-            GPIO.setup(self.p_power, GPIO.OUT)
-            GPIO.output(self.p_power, GPIO.LOW)
+            GPIO.setup(self.p_power, GPIO.OUT) # pylint: disable=E0606
+            GPIO.output(self.p_power, GPIO.LOW) # pylint: disable=E0606
             
         self.name = name
 
@@ -68,7 +68,7 @@ class SensorPower:
         if sys.platform == "esp32":
             self.pin_power.value(power)
         else:
-            GPIO.output(self.p_power, power)
+            GPIO.output(self.p_power, power) # pylint: disable=E0606
 
 #    def __str__(self):
 #        return ("{}Pin# driver control: {:2}, Pin# driver status: {:2}, Status: {:>10}"
