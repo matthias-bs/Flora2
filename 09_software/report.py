9799190928235acbb32658ea76e1dbcf61f05b1a
Matthias Prinke <m.prinke@arcor.de>
2025-04-06 22:04:39 +0200

    Code improvements

--- 09_software/report.py
+++ 09_software/report.py
@@ -2,7 +2,7 @@
 # report.py
 #
 # This module provides the Report class
-# 
+#
 # - generates HTML report with various sensor/plant and system data
 #
 # created: 01/2021 updated: 06/2021
@@ -21,7 +21,7 @@
 # 20210207 Changed to send email in smaller chunks
 #          due to ESP memory constraints
 # 20210316 Added reduced sensor status report in case only
-#          analog moisture sensors are used 
+#          analog moisture sensors are used
 # 20210321 Changed 'analog' sensors to 'local' sensors in configuration,
 #          added option for temperature sensor,
 #          added garbage collection
@@ -30,26 +30,25 @@
 # 20240402 Code improvements
 #
 # Backlog:
-# - 
+# -
 #
 ###############################################################################
+"""Report module."""
 
 import json
-
+from time import localtime
 from config import config
 import sensor
 import pump
 import tank
-from time import localtime
-
 
 ###############################################################################
-# Report class - Generate status report  
+# Report class - Generate status report
 ###############################################################################
 class Report:
     """
     Generate status report
-    
+
     Attributes:
         email         (Email):      instance of Email class
         min_light_irr (int):        smallest light intensity which still allows irrigation
@@ -58,16 +57,16 @@ class Report:
         """
         The constructor for Report class.
         """
-        
+
         self.data = {}
         self.min_light_irr = 1000000
-        
+
     def gen_report(self):
         """
         Generate report
         """
         self.data['timestamp'] = self.date_time_str(localtime())
-        
+
         # Find minimum light_irr value of all sensors
         self.min_light_irr = 1000000
         for _, s in sensor.sensors.items():
@@ -81,10 +80,10 @@ class Report:
     def date_time_str(self, dt):
         """
         Output date and time as string
-        
+
         Parameters:
             dt (date-time tuple): (year, month, mday, hour, minute, second, weekday, yearday)
-            
+
         Returns:
             string: fixed format dd.mm.yy hh:mm
         """
@@ -118,7 +117,7 @@ class Report:
                 if config.temperature_sensor:
                     self.data[s.name]['status']['temp_ul'] = s.temp_ul
                     self.data[s.name]['status']['temp_oh'] = s.temp_oh
-                
+
                 self.data[s.name]['status']['moist_ul'] = s.moist_ul
                 self.data[s.name]['status']['moist_ll'] = s.moist_ll
                 self.data[s.name]['status']['moist_hl'] = s.moist_hl
@@ -134,17 +133,21 @@ class Report:
         """
         self.data['irrigation'] = []
         for i, p in enumerate(pump.pumps):
-            if (p.timestamp != 0):
+            if p.timestamp != 0:
                 last_irrigation = self.date_time_str(localtime(p.timestamp))
                 next_irrigation = self.date_time_str(localtime(p.timestamp + config.irr_rest))
                 scheduled = config.irr_scheduled[i]
-                self.data['irrigation'].append({'last': last_irrigation, 'next': next_irrigation, 'scheduled': scheduled})
+                self.data['irrigation'].append({
+                    'last': last_irrigation,
+                    'next': next_irrigation, 
+                    'scheduled': scheduled
+                })
 
         self.data['pump'] = []
         for p in pump.pumps:
-            if (p.status == 2):
+            if p.status == 2:
                 status = "on: error"
-            elif (p.status == 4):
+            elif p.status == 4:
                 status = "off: error"
             else:
                 status = "ok"
