f8c50d5d84da54a350c0cbd6d545ce86fcfb1825
Matthias Prinke <m.prinke@arcor.de>
2025-04-08 18:08:09 +0200

    Coding style improvements

--- 09_software/print_line.py
+++ 09_software/print_line.py
@@ -16,20 +16,24 @@
 # History:
 #
 # 20210117 Extracted from flora.py
-# 20250402 Code improvements
+# 20250402 Coding style improvements
+# 20250408 Coding style improvements
 #
 # Backlog:
 # -
 #
 ###############################################################################
+"""Logging module"""
 
 import sys
+from time import localtime
+
 if sys.implementation.name != "micropython":
     import sdnotify
     from unidecode import unidecode
     from colorama import Fore, Style
     from time import strftime
-from time import localtime
+
 
 ###################################################################################
 # Logging function
@@ -52,26 +56,31 @@ if sys.implementation.name != "micropython":
             error (bool):     format console output as error
             warning (bool):   format console output as warning
             sd_notify (bool): generate systemd sd_notify protocol output
-            console (bool):   generate console output (with formatting depending on flags error/warning)
-        """    
+            console (bool):   generate console output
+                              (with formatting depending on flags error/warning)
+        """
         timestamp = strftime('%Y-%m-%d %H:%M:%S', localtime())
         if console:
             if error:
-                print(Fore.RED + Style.BRIGHT + '[{}] '.format(timestamp) + Style.RESET_ALL + '{}'.format(text) + Style.RESET_ALL, file=sys.stderr)
+                print(Fore.RED + Style.BRIGHT + f'[{timestamp}] ' + Style.RESET_ALL + \
+                      f'{text}' + Style.RESET_ALL, file=sys.stderr)
             elif warning:
-                print(Fore.YELLOW + '[{}] '.format(timestamp) + Style.RESET_ALL + '{}'.format(text) + Style.RESET_ALL)
+                print(Fore.YELLOW + f'[{timestamp}] ' + Style.RESET_ALL + \
+                      f'{text}' + Style.RESET_ALL)
             else:
-                print(Fore.GREEN + '[{}] '.format(timestamp) + Style.RESET_ALL + '{}'.format(text) + Style.RESET_ALL)
+                print(Fore.GREEN + f'[{timestamp}] ' + Style.RESET_ALL + \
+                      f'{text}' + Style.RESET_ALL)
         timestamp_sd = strftime('%b %d %H:%M:%S', localtime())
         if sd_notify:
-            sd_notifier.notify('STATUS={} - {}.'.format(timestamp_sd, unidecode(text)))
+            sd_notifier.notify(f'STATUS={timestamp_sd} - {unidecode(text)}.')
 else:
     def print_line(text, error = False, warning=False, sd_notify=False, console=True): # pylint: disable=unused-argument
+        """Simplified logging function for MicroPython"""
 
         dt = localtime()
         # date-time = (year, month, mday, hour, minute, second, weekday, yearday)
         # timestamp = yyyy-mm-dd HH:MM:SS
-        timestamp = '{}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}'.format(dt[0], dt[1], dt[2], dt[3], dt[4], dt[5])
+        timestamp = f'{dt[0]}-{dt[1]:02d}-{dt[2]:02d} {dt[3]:02d}:{dt[4]:02d}:{dt[5]:02d}'
 
         #timestamp = strftime('%Y-%m-%d %H:%M:%S', localtime())
-        print('[{}] '.format(timestamp) + '{}'.format(text))
+        print(f'[{timestamp}] {text}')
